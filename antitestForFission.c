49bf3e8180655ae714b1a72b0fbef79bbfa0780d
nevenani <144362574+nevenani@users.noreply.github.com>
2026-08-28 20:15:10 +0200

    Create antitestForFission.c

---
+++ antitestForFission.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+for (int i = 0; i < n; i++) {
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+
+    if (x < 10) {
+        y++;
+    } else {
+        y--;
+    }
+}
+    printf("Hello World, I'm dooing loop fission!\n");
+    return 0;
+}
